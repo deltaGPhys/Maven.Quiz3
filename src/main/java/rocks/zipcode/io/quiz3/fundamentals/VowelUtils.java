bf8a99fbee54e81d7b8818818ea97b7163bf140e
deltagphys <cty.jgates@gmail.com>
2019-11-18 13:39:54 -0500

    first sction

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
@@ -1,23 +1,40 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class VowelUtils {
     public static Boolean hasVowels(String word) {
-        return null;
+        for (String letter : word.split("")) {
+            if (VowelUtils.isVowel(letter.charAt(0))) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+        String[] letters = word.split("");
+        for (int i = 0; i < letters.length; i++) {
+            if (VowelUtils.isVowel(letters[i].charAt(0))){
+                return i;
+            }
+        }
+        return -1;
     }
 
 
     public static Boolean startsWithVowel(String word) {
-        return null;
+        if (VowelUtils.isVowel(word.charAt(0))){
+            return true;
+        }
+        return false;
     }
 
     public static Boolean isVowel(Character character) {
-        return null;
+        return character.toString().matches("^[aAeEiIoOuU]$");
     }
 }
