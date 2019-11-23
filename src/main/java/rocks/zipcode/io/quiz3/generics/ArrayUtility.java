94dae2fc8371c5807ca1cb4bf1dd798be8f87277
deltagphys <cty.jgates@gmail.com>
2019-11-23 10:25:56 -0500

    better generic arrayutil

--- src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
+++ src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
@@ -1,8 +1,13 @@
 package rocks.zipcode.io.quiz3.generics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toList;
 
 /**
  * @author leon on 09/12/2018.
@@ -27,11 +32,18 @@ public class ArrayUtility<SomeType> {
     }
 
     public SomeType[] filter(Function<SomeType, Boolean> predicate) {
-        Object[] results = Arrays.stream(array).filter(predicate::apply).toArray();
-        SomeType[] target = Arrays.copyOf(array, results.length);
-        for (int i = 0; i < results.length; i++) {
-            target[i] = (SomeType) results[i];
-        }
-        return target;
+//        Object[] results = Arrays.stream(array).filter(predicate::apply).toArray();
+//        SomeType[] target = Arrays.copyOf(array, results.length);
+//        for (int i = 0; i < results.length; i++) {
+//            target[i] = (SomeType) results[i];
+//        }
+//        return target;
+
+//        ArrayList<SomeType> target = new ArrayList<SomeType>();
+//        Arrays.stream(array).filter(predicate::apply).forEach(x -> target.add(x));
+//        return target.toArray(Arrays.copyOf(array, 0));
+
+        return Arrays.stream(array).filter(predicate::apply).collect(toList()).toArray(Arrays.copyOf(array, 0));
+
     }
 }
